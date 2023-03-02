3b481b918e9f455e4fec6bf834dbe69bd27838c0
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-03-02 17:46:09 -0300

    Adição do Header, Footer e container na Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './components/header/index.jsx'
+import Footer from './components/footer/index.jsx'
+import Icon from './assets/img/logo.png'
 
+function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+      <Header />
+
+      <main className='main'>
+        <form className='form'>
+          <div className='icon'>
+            <img src={Icon} alt="Logo" width='30%'/>
+          </div>
+          <div className='field'> 
+            <span>CEP</span>
+            <input type="text" name='cep' className='input' placeholder='Digite o CEP'/>
+          </div>
+        </form>
+      </main>
+
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export default function Footer() {
+    return(
+        <footer className='footer'>
+            <span>👨‍💻 Luan Patrik 👨‍💻</span>
+        </footer>
+    )
+}
