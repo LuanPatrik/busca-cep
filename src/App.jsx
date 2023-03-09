91f6d52ba7f81a7eca03976bde86aaa3fad86223
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-03-09 17:22:45 -0300

    Finalização do projeto!

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import './App.css'
 
 import Header from './components/header/index.jsx'
@@ -6,6 +6,32 @@ import Footer from './components/footer/index.jsx'
 import Icon from './assets/img/logo.png'
 
 function App() {
+  const { register, setValue, setFocus } = useForm()
+
+  function checkCep(e) {
+    const cep = e.target.value;
+    console.log(cep)
+    if (cep.length !== 8) {
+      return
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response)=>response.json())
+    .then((data) => {
+      setValue('cidade', data.localidade)
+      setValue('bairro', data.bairro)
+      setValue('estado', data.uf)
+      setValue('rua', data.logradouro)
+      if (data.logradouro) {
+        setFocus('numero')        
+      }else{
+        setFocus('rua')
+      }
+    })
+    
+  }
+
+
   return (
     <div className="container">
       <Header />
@@ -15,9 +41,61 @@ function App() {
           <div className='icon'>
             <img src={Icon} alt="Logo" width='30%'/>
           </div>
+
           <div className='field'> 
             <span>CEP</span>
-            <input type="text" name='cep' className='input' placeholder='Digite o CEP'/>
+            <input 
+            maxLength={8}
+              type="text" 
+              name='cep' 
+              className='input' 
+              {...register('cep')} 
+              onBlur={checkCep}/>
+          </div>
+
+          <div className='field'> 
+            <span>Rua</span>
+            <input 
+              type="text" 
+              name='rua' 
+              className='input' 
+              {...register('rua')}/>
+          </div>
+
+          <div className='field'> 
+            <span>Número</span>
+            <input 
+              type="text" 
+              name='numero' 
+              className='input' 
+              {...register('numero')}/>
+          </div>
+
+          <div className='field'> 
+            <span>Bairro</span>
+            <input 
+              type="text" 
+              name='bairro' 
+              className='input' 
+              {...register('bairro')}/>
+          </div>
+
+          <div className='field'> 
+            <span>Cidade</span>
+            <input 
+              type="text" 
+              name='cidade' 
+              className='input' 
+              {...register('cidade')}/>
+          </div>
+
+          <div className='field'> 
+            <span>Estado</span>
+            <input 
+              type="text" 
+              name='estado' 
+              className='input' 
+              {...register('estado')}/>
           </div>
         </form>
       </main>
